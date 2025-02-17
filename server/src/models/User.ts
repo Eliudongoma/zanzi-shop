73c95aa41d4bfa60786a9144f2b6bfe583d936cf
Eliudongoma <ongomaeliud5@gmail.com>
2025-02-17 21:43:06 +0300

    Added orders and user models on the server side

---
+++ server/src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword: string){
+  return await bcrypt.compare(candidatePassword, this.password);
+}
+export default mongoose.model("User", userSchema);
